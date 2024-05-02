c4f9c6122791b41ff99db22d1df1a17864ff73d3
Florian Klampfer <mail@qwtel.com>
2024-05-02 12:46:40 +0700

    Commit

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,11 @@ import { SQLiteEditorDefaultProvider, SQLiteEditorOptionProvider  } from './sqli
 import TelemetryReporter from '@vscode/extension-telemetry';
 // import { Credentials } from './credentials';
 
-const key = "36072a93-f98f-4c93-88c3-8870add45a57";
+const ExtensionId = 'qwtel.sqlite-viewer'
+const Key = "36072a93-f98f-4c93-88c3-8870add45a57";
 
 export async function activate(context: vsc.ExtensionContext) {
-  const reporter = new TelemetryReporter(key);
+  const reporter = new TelemetryReporter(Key);
   // const credentials = new Credentials(context);
   context.subscriptions.push(SQLiteEditorDefaultProvider.register(context, reporter /* , credentials */));
   context.subscriptions.push(SQLiteEditorOptionProvider.register(context, reporter /* , credentials */));
@@ -15,56 +16,58 @@ export async function activate(context: vsc.ExtensionContext) {
   showWhatsNew(context, reporter);
 }
 
-const extensionId = 'qwtel.sqlite-viewer'
+// https://stackoverflow.com/a/66303259/3073272
+function isMajorUpdate(currVersion: string, prevVersion?: string|null) {
+  if (!prevVersion?.includes('.')) {
+    return true;
+  }
+  //returns int array [1,1,1] i.e. [major,minor,patch]
+  var prev = prevVersion.split('-')[0].split('.').map(Number);
+  var curr = currVersion.split('-')[0].split('.').map(Number);
 
-// // https://stackoverflow.com/a/66303259/3073272
-// function isMajorUpdate(previousVersion: string, currentVersion: string) {
-//   // rain-check for malformed string
-//   if (previousVersion.indexOf('.') === -1) {
-//     return true;
-//   }
-//   //returns int array [1,1,1] i.e. [major,minor,patch]
-//   var previousVerArr = previousVersion.split('.').map(Number);
-//   var currentVerArr = currentVersion.split('.').map(Number);
-
-//   if (currentVerArr[0] > previousVerArr[0]) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+  if (curr[0] > prev[0] || (curr[0] === 0 && prev[0] === 0 && curr[1] > prev[1])) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 async function showWhatsNew(context: vsc.ExtensionContext, reporter: TelemetryReporter) {
-  const previousVersion = context.globalState.get<string>(extensionId);
-  const currentVersion = vsc.extensions.getExtension(extensionId)!.packageJSON.version;
+  const prevVersion = context.globalState.get<string>(ExtensionId);
+  const currVersion = vsc.extensions.getExtension(ExtensionId)!.packageJSON.version as string;
 
   // store latest version
-  context.globalState.update(extensionId, currentVersion);
+  context.globalState.update(ExtensionId, currVersion);
 
   if (
-    previousVersion === undefined || 
-    previousVersion !== currentVersion || 
+    prevVersion === undefined || 
+    prevVersion !== currVersion || 
     context.extensionMode === vsc.ExtensionMode.Development
   ) {
     reporter.sendTelemetryEvent("install");
 
-    let actions;
-    const result = await vsc.window.showInformationMessage(
-      `SQLite Viewer updated to new major version! Check out the Changelog to see what's new!`,
-      ...actions = [{ title: 'Open Changelog ↗'}]
-    );
+    if (
+      isMajorUpdate(currVersion, prevVersion) ||
+      context.extensionMode === vsc.ExtensionMode.Development
+    ) {
+      let actions;
+      const result = await vsc.window.showInformationMessage(
+        `SQLite Viewer updated to new major version! Check out the Changelog to see what's new!`,
+        ...actions = [{ title: 'Open Changelog ↗'}]
+      );
 
-    if (result !== null) {
-      if (result === actions[0]) {
-        await vsc.env.openExternal(
-          vsc.Uri.parse('https://marketplace.visualstudio.com/items/qwtel.sqlite-viewer/changelog')
-        );
+      if (result !== null) {
+        if (result === actions[0]) {
+          await vsc.env.openExternal(
+            vsc.Uri.parse('https://marketplace.visualstudio.com/items/qwtel.sqlite-viewer/changelog')
+          );
+        }
+        // else if (result === actions[1]) {
+        //   await vsc.env.openExternal(
+        //     vsc.Uri.parse('https://github.com/qwtel/sqlite-viewer-vscode/issues')
+        //   );
+        // }
       }
-      // else if (result === actions[1]) {
-      //   await vsc.env.openExternal(
-      //     vsc.Uri.parse('https://github.com/qwtel/sqlite-viewer-vscode/issues')
-      //   );
-      // }
     }
   }
 }
