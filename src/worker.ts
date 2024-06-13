dd554cc54155a2e99f4e679cb52692036a4b74d2
Florian Klampfer <mail@qwtel.com>
2024-06-13 13:04:20 +0700

    WIP

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,6 @@
 import { parentPort } from "worker_threads";
 import * as Comlink from "../sqlite-viewer-core/src/comlink";
 import nodeEndpoint from "../sqlite-viewer-core/src/vendor/comlink/src/node-adapter";
+import { WASMDbWrapperFactory } from "../sqlite-viewer-core/src/worker-db-factory"; // TODO: make a more specialized factory for vscode??
 import { WorkerDB } from "../sqlite-viewer-core/src/worker-db";
-
-Comlink.expose(new WorkerDB(), nodeEndpoint(parentPort!))
+Comlink.expose(new WorkerDB(new WASMDbWrapperFactory()), nodeEndpoint(parentPort!))
