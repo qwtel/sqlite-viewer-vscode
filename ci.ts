6b90a12eb9c7778b816e8acb10c4d067bdc0369a
Florian Klampfer <mail@qwtel.com>
2024-10-13 14:42:56 +0700

    Commit
    # Conflicts:
    #	package.json

--- ci.ts
+++ ci.ts
@@ -1,9 +1,12 @@
 /// <reference types="bun-types" />
 
+import { parseArgs } from "node:util";
 import { packageExt } from "./pack";
 
 const DEV = !!Bun.env.DEV;
 console.log({ DEV })
+
+const kinds = ["package", "publish"] as const
 
 const matrix = [
   "win32-x64",
@@ -18,9 +21,29 @@ const matrix = [
   "web",
 ] as const;
 
-for (const [i, target] of matrix.entries()) {
-  await packageExt(
-    { target, kind: "publish" }, 
-    i === 0 ? Bun.env : { ...Bun.env, VSCODE_EXT_SKIP_COMPILE: "1" },
-  );
+if (import.meta.main) {
+  const args = parseArgs({
+    args: Bun.argv,
+    options: {
+      tool: { type: 'string' },
+      kind: { type: 'string' },
+      "pre-release": { type: 'boolean' },
+    },
+    strict: true,
+    allowPositionals: true,
+  });
+
+  let { tool, kind, 'pre-release': preRelease } = args.values;
+
+  tool ||= "vsce";
+
+  if (kind && !kinds.includes(kind as any)) {
+    throw new Error(`Invalid kind: ${kind}. Must be one of: ${kinds.join(', ')}`);
+  }
+  kind ||= "package";
+
+  for (const [i, target] of matrix.entries()) {
+    const env = i === 0 ? Bun.env : { ...Bun.env, VSCODE_EXT_SKIP_COMPILE: "1" }
+    await packageExt({ tool, kind, target, 'pre-release': preRelease }, env);
+  }
 }
