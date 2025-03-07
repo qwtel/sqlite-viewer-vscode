4aa460c2db78af8a53950625741f6e3b38d87251
Florian Klampfer <mail@qwtel.com>
2025-03-07 21:38:19 +0100

    Commit

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,6 @@ export const Ns = 'qwtel';
 export const ExtensionId = 'sqlite-viewer';
 export const FullExtensionId = `${Ns}.${ExtensionId}`
 
-export const LANGS: ('en'|'de'|'fr'|'pt-br'|'ja'|'es'|'ko')[] = ['en', 'de', 'fr', 'pt-br', 'ja', 'es', 'ko'];
-
 export const ConfigurationSection = 'sqliteViewer';
 
 export const TelemetryConnectionString = "InstrumentationKey=36072a93-f98f-4c93-88c3-8870add45a57;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=5ef71db6-3a49-4597-ad99-b159b2b49125";
