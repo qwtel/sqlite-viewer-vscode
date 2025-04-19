4aef9b84401754086e1952f636fb5498a143b90e
Florian Klampfer <mail@qwtel.com>
2025-04-19 17:59:03 +0200

    Revert "Commit"
    This reverts commit 76a0938174a9593b6f7742b74b02b921bd011dc0.
    
    # Conflicts:
    #	CHANGELOG.md
    #	sqlite-viewer-core

--- src/commands.ts
+++ src/commands.ts
@@ -214,12 +214,10 @@ export async function refreshAccessToken(context: vsc.ExtensionContext, licenseK
   return data.token;
 }
 
-const jwtKeyPromise = jose.importSPKI(JWTPublicKeySPKI, 'ES256')
-jwtKeyPromise.catch(() => {});
-
 export async function verifyToken<PayloadType = jose.JWTPayload>(accessToken: string): Promise<PayloadType & jose.JWTPayload|null> {
   try {
-    const { payload } = await jose.jwtVerify<PayloadType>(accessToken, await jwtKeyPromise);
+    const jwtKey = await jose.importSPKI(JWTPublicKeySPKI, 'ES256');
+    const { payload } = await jose.jwtVerify<PayloadType>(accessToken, jwtKey);
     return payload;
   } catch {
     return null;
